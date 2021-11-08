4ce3d12a6260edfdb928d57e2b3ae664c2795dfa
max9901 <mcwengelen@gmail.com>
2021-11-08 17:26:14 +0100

    custom compilation of CXX catalogues:

--- mechanisms/BuildModules.cmake
+++ mechanisms/BuildModules.cmake
@@ -37,11 +37,11 @@ function(build_modules)
         endif()
 
         add_custom_command(
-            OUTPUT ${generated}
-            DEPENDS ${depends}
-            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-            COMMAND ${modcc_bin} ${flags} ${mod}
-            COMMENT "modcc generating: ${generated}"
+                OUTPUT ${generated}
+                DEPENDS ${depends}
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                COMMAND ${modcc_bin} ${flags} ${mod}
+                COMMENT "modcc generating: ${generated}"
         )
         set_source_files_properties(${generated} PROPERTIES GENERATED TRUE)
         list(APPEND all_generated ${generated})
@@ -58,7 +58,7 @@ function(build_modules)
 endfunction()
 
 function("make_catalogue")
-  cmake_parse_arguments(MK_CAT "" "NAME;SOURCES;OUTPUT;PREFIX;STANDALONE;VERBOSE" "CXX_FLAGS_TARGET;MECHS" ${ARGN})
+  cmake_parse_arguments(MK_CAT "" "NAME;SOURCES;OUTPUT;PREFIX;STANDALONE;VERBOSE;CXXIN" "CXX_FLAGS_TARGET;MECHS" ${ARGN})
   set(MK_CAT_OUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/generated/${MK_CAT_NAME}")
 
   # Need to set ARB_WITH_EXTERNAL_MODCC *and* modcc
@@ -77,62 +77,69 @@ function("make_catalogue")
     message("Arbor cxx compiler:   ${ARB_CXX}")
     message("Script prefix:        ${MK_CAT_PREFIX}")
     message("Current cxx compiler: ${CMAKE_CXX_COMPILER}")
+    message("CXXIN:                ${MK_CAT_CXXIN}")
   endif()
-
-  file(MAKE_DIRECTORY "${MK_CAT_OUT_DIR}")
 
   if (NOT TARGET build_all_mods)
-    add_custom_target(build_all_mods)
+      add_custom_target(build_all_mods)
   endif()
 
-  build_modules(
-    ${MK_CAT_MECHS}
-    SOURCE_DIR "${MK_CAT_SOURCES}"
-    DEST_DIR "${MK_CAT_OUT_DIR}"
-    ${external_modcc} # NB: expands to 'MODCC <binary>' to add an optional argument
-    MODCC_FLAGS -t cpu -t gpu ${ARB_MODCC_FLAGS} -N arb::${MK_CAT_NAME}_catalogue
-    GENERATES .hpp _cpu.cpp _gpu.cpp _gpu.cu
-    TARGET build_catalogue_${MK_CAT_NAME}_mods)
+  if(MK_CAT_CXXIN)
+    #  todo copy and check cpp files here
+    set(MK_CAT_OUT_DIR ${MK_CAT_SOURCES})
+    add_custom_target(build_catalogue_${MK_CAT_NAME}_mods)
+  else()
+    file(MAKE_DIRECTORY "${MK_CAT_OUT_DIR}")
+    build_modules(
+      ${MK_CAT_MECHS}
+      SOURCE_DIR "${MK_CAT_SOURCES}"
+      DEST_DIR "${MK_CAT_OUT_DIR}"
+      ${external_modcc} # NB: expands to 'MODCC <binary>' to add an optional argument
+      MODCC_FLAGS -t cpu -t gpu ${ARB_MODCC_FLAGS} -N arb::${MK_CAT_NAME}_catalogue
+      GENERATES .hpp _cpu.cpp _gpu.cpp _gpu.cu
+      TARGET build_catalogue_${MK_CAT_NAME}_mods)
+  endif()
 
+# All original from here:
   set(catalogue_${MK_CAT_NAME}_source ${CMAKE_CURRENT_BINARY_DIR}/${MK_CAT_NAME}_catalogue.cpp)
   set(catalogue_${MK_CAT_NAME}_options -A arbor -I ${MK_CAT_OUT_DIR} -o ${catalogue_${MK_CAT_NAME}_source} -B multicore -C ${MK_CAT_NAME} -N arb::${MK_CAT_NAME}_catalogue)
   if(ARB_WITH_GPU)
-    list(APPEND catalogue_${MK_CAT_NAME}_options -B gpu)
+      list(APPEND catalogue_${MK_CAT_NAME}_options -B gpu)
   endif()
 
   add_custom_command(
-    OUTPUT  ${catalogue_${MK_CAT_NAME}_source}
-    COMMAND ${MK_CAT_PREFIX}/generate_catalogue ${catalogue_${MK_CAT_NAME}_options} ${MK_CAT_MECHS}
-    COMMENT "Building catalogue ${MK_CAT_NAME}"
-    DEPENDS ${MK_CAT_PREFIX}/generate_catalogue)
+          OUTPUT  ${catalogue_${MK_CAT_NAME}_source}
+          COMMAND ${MK_CAT_PREFIX}/generate_catalogue ${catalogue_${MK_CAT_NAME}_options} ${MK_CAT_MECHS}
+          COMMENT "Building catalogue ${MK_CAT_NAME}"
+          DEPENDS ${MK_CAT_PREFIX}/generate_catalogue)
 
   add_custom_target(${MK_CAT_NAME}_catalogue_cpp_target DEPENDS ${catalogue_${MK_CAT_NAME}_source})
   add_dependencies(build_catalogue_${MK_CAT_NAME}_mods ${MK_CAT_NAME}_catalogue_cpp_target)
   add_dependencies(build_all_mods build_catalogue_${MK_CAT_NAME}_mods)
 
   foreach(mech ${MK_CAT_MECHS})
-    list(APPEND catalogue_${MK_CAT_NAME}_source ${MK_CAT_OUT_DIR}/${mech}_cpu.cpp)
-    if(ARB_WITH_GPU)
-      list(APPEND catalogue_${MK_CAT_NAME}_source ${MK_CAT_OUT_DIR}/${mech}_gpu.cpp ${MK_CAT_OUT_DIR}/${mech}_gpu.cu)
-    endif()
+      list(APPEND catalogue_${MK_CAT_NAME}_source ${MK_CAT_OUT_DIR}/${mech}_cpu.cpp)
+      if(ARB_WITH_GPU)
+          list(APPEND catalogue_${MK_CAT_NAME}_source ${MK_CAT_OUT_DIR}/${mech}_gpu.cpp ${MK_CAT_OUT_DIR}/${mech}_gpu.cu)
+      endif()
   endforeach()
   set(${MK_CAT_OUTPUT} ${catalogue_${MK_CAT_NAME}_source} PARENT_SCOPE)
 
   if(${MK_CAT_STANDALONE})
-    add_library(${MK_CAT_NAME}-catalogue SHARED ${catalogue_${MK_CAT_NAME}_source})
-    target_compile_definitions(${MK_CAT_NAME}-catalogue PUBLIC STANDALONE=1)
-    target_compile_options(${MK_CAT_NAME}-catalogue PUBLIC ${MK_CAT_CXX_FLAGS_TARGET})
-    set_target_properties(${MK_CAT_NAME}-catalogue
-      PROPERTIES
-      SUFFIX ".so"
-      PREFIX ""
-      CXX_STANDARD 17)
-    target_include_directories(${MK_CAT_NAME}-catalogue PUBLIC "${MK_CAT_ARBOR}/arbor/")
+      add_library(${MK_CAT_NAME}-catalogue SHARED ${catalogue_${MK_CAT_NAME}_source})
+      target_compile_definitions(${MK_CAT_NAME}-catalogue PUBLIC STANDALONE=1)
+      target_compile_options(${MK_CAT_NAME}-catalogue PUBLIC ${MK_CAT_CXX_FLAGS_TARGET})
+      set_target_properties(${MK_CAT_NAME}-catalogue
+              PROPERTIES
+              SUFFIX ".so"
+              PREFIX ""
+              CXX_STANDARD 17)
+      target_include_directories(${MK_CAT_NAME}-catalogue PUBLIC "${MK_CAT_ARBOR}/arbor/")
 
-    if(TARGET arbor)
-      target_link_libraries(${MK_CAT_NAME}-catalogue PRIVATE arbor)
-    else()
-      target_link_libraries(${MK_CAT_NAME}-catalogue PRIVATE arbor::arbor)
-    endif()
+      if(TARGET arbor)
+          target_link_libraries(${MK_CAT_NAME}-catalogue PRIVATE arbor)
+      else()
+          target_link_libraries(${MK_CAT_NAME}-catalogue PRIVATE arbor::arbor)
+      endif()
   endif()
 endfunction()
